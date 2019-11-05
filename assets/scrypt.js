6e729721ee1b5f0b627b05b6eacd7b9accf0ddec
Khaoula <AlKhaoula02@gmail.com>
2019-11-05 14:50:44 +0100

    dispo

--- assets/scrypt.js
+++ assets/scrypt.js
@@ -1,18 +1,41 @@
 let pic = document.querySelector("img");
 let color = document.querySelectorAll(".couleurs ul li");
-let montitre = document.createElement("h2");
+let montitre = document.querySelector(".titre h2");
 let colortitre = ["Red", "Blue", "Grey"];
+let price =[25.99,27.99,30.99];
+let monprix= document.querySelector(".prix h2");
+
+
+
 
 for (let i = 0; i < color.length; i++) {
     const element = color[i];
     element.addEventListener("click", function () {
         // `${fezfz}`;
         pic.src = `img/nike-${element.classList[0]}.jpg`;
-        
-        document.querySelectorAll(".titre").appendChild(montitre);
-        montitre.innerHTML = `Nike${colortitre[i]}`;
+        montitre.innerHTML = `Nike ${colortitre[i]}`;
+        monprix.innerHTML = `${price[i]} Euros`;
         console.log(colortitre[i]);
-
     });
+}
+
+
+let size = document.querySelectorAll(".tailles ul li");
+let stock = document.querySelector(".stock p");
+let button= document.querySelectorAll("boutton");
+
+for (let i = 0; i < size.length; i++) {
+
+size[i].addEventListener('click', function () {
+
+if (i == 3) {
+stock.innerHTML = "Plus disponible";
+button.classList.add("disabled");
+}
+else {
+stock.innerHTML = "Disponible";
+button.classList.remove("disabled");
+}
+})
 }
 
